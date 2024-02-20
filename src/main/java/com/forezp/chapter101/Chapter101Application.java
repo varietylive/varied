9cd37ce2bc6eda9915ec04d5210670e98700fb70
kk <z260544@163.com>
2024-02-20 21:03:08 +0800

    git仓库初次提交

---
+++ src/main/java/com/forezp/chapter101/Chapter101Application.java
@@ -0,0 +1,13 @@
+package com.forezp.chapter101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/forezp/chapter101/Chapter101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forezp.chapter101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
